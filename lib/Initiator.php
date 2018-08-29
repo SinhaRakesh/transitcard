92588b86465228560a1344fc58cd245aa1b6ab23
sinharakesh <rksinha.btech@gmail.com>
2018-08-29 09:23:19 +0530

    initial commit

---
+++ lib/Initiator.php
@@ -0,0 +1,40 @@
+
+<?php
+
+namespace rakesh\apartment;
+
+class Initiator extends \Controller_Addon {
+    public $addon_name = 'r_apartment';
+
+    function setup_admin(){
+
+    	if($this->app->is_admin){
+            $this->routePages('rakesh_apartment');
+            $this->addLocation(array('template'=>'templates','js'=>'templates/js','css'=>['templates/css','templates/js']))
+            ->setBaseURL('../shared/apps/rakesh/');
+        }
+
+        $m = $this->app->top_menu->addMenu('Apartment');
+        
+    	return $this;
+    }
+
+    function setup_pre_frontend(){
+        $this->routePages('rakesh_apartment');
+        $this->addLocation(array('template'=>'templates','js'=>'templates/js','css'=>['templates/css','templates/js']))
+        ->setBaseURL('./shared/apps/rakesh/apartment/');
+
+    }
+    
+    function setup_frontend(){
+        $this->routePages('rakesh_apartment');
+        $this->addLocation(array('template'=>'templates','js'=>'templates/js','css'=>['templates/css','templates/js']))
+        ->setBaseURL('./shared/apps/rakesh/apartment/');
+        
+        $this->app->exportFrontEndTool('rakesh\apartment\Tool_Applicationform','apartment');
+        $this->app->exportFrontEndTool('rakesh\apartment\Tool_Course','apartment');
+        $this->app->exportFrontEndTool('rakesh\apartment\Tool_Login','apartment');
+        $this->app->exportFrontEndTool('rakesh\apartment\Tool_Carousel','apartment');        
+    	return $this;
+    }
+}
