ed33cf5b65db5266c02c57d83036e93378ea0c4b
sinharakesh <rksinha.btech@gmail.com>
2018-09-09 13:13:21 +0530

    view apartment updated

--- lib/Tool/Dashboard.php
+++ lib/Tool/Dashboard.php
@@ -9,17 +9,29 @@ class Tool_Dashboard extends \xepan\cms\View_Tool{
 	function init(){
 		parent::init();
 
+		$this->app->stickyGET('mode');
+		// $menu = $this->add('Menu');
+		// $menu->addMenuItem($this->app->url(null,['mode'=>'apartment']),'Apartment');
+		// $menu->addMenuItem($this->app->url(null,['mode'=>'flat']),'Flat');
+
 		if($this->owner instanceof \AbstractController){
 			$this->add('View_Info')->set('dashboard tool, reload');
 			return;
 		}
-
+		
 		if(!@$this->app->apartment->id){
 			$this->add('View_Info')->set('First Update Your Apartment info');
 			$this->add('rakesh\apartment\View_Apartment');
 		}
 
-		$this->add('rakesh\apartment\View_Flat');
+		switch ($_GET['mode']){
+			case 'apartment':
+				$this->add('rakesh\apartment\View_Apartment')->addClass('card');
+				break;
+			case 'flat':
+				$this->add('rakesh\apartment\View_Flat')->addClass('card');
+				break;
+		}
 
 	}
 }
--- lib/View/Apartment.php
+++ lib/View/Apartment.php
@@ -8,6 +8,8 @@ class View_Apartment extends \View{
 	function init(){
 		parent::init();
 		
+		$this->app->template->set('page_title','Apartment Profile & Setting');
+
 		$model = $this->app->apartment;	
 		$model_id = @$this->app->apartment->id;	
 		if(!$model_id){
@@ -17,8 +19,26 @@ class View_Apartment extends \View{
 		}
 
 		$form = $this->add('Form');
-		$form->setModel($model);
-		$form->addSubmit('Submit');
+		$form->add('xepan\base\Controller_FLC')
+			->makePanelsCoppalsible(true)
+			->addContentSpot()
+			->layout([
+				'name~Your Apartment Name'=>'Apartment Information~c1~12',
+				'country_id~Country'=>'c31~3',
+				'state_id~State'=>'c32~3',
+				'city'=>'c33~3',
+				'address'=>'c34~3',
+				'view~<hr/><h3>Billing Dates</h3>'=>'c41~12',
+				'bill_generation_date'=>'c21~6',
+				'last_submission_date'=>'c22~6',
+				'view1~<hr/><h3>Builder Information</h3>'=>'c51~12',
+				'builder_name'=>'c11~4',
+				'builder_mobile_no'=>'c12~4',
+				'builder_email_id'=>'c13~4'
+			]);
+
+		$form->setModel($model,['name','city','address','builder_name','builder_email_id','builder_mobile_no','state_id','country_id','bill_generation_date','last_submission_date']);
+		$form->addSubmit('Update Informartion')->addClass('btn btn-primary text-center');
 
 		if($form->isSubmitted()){
 			$form->save();
@@ -26,7 +46,7 @@ class View_Apartment extends \View{
 			$this->app->apartmentmember['apartment_id'] = $apartment_model->id;
 			$this->app->apartmentmember->save();
 
-			$form->js(null,$form->js()->reload())->univ()->successMessage('Your Apartment Added Successfully')->execute();
+			$form->js(null,$form->js()->reload())->univ()->successMessage('Apartment Information Updated Successfully')->execute();
 		}
 
 
