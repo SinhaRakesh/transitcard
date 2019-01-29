de6d0139f31e4f32113cba92edfd3ec28a09cf4b
sinharakesh <rksinha.btech@gmail.com>
2019-01-29 11:00:07 +0530

    last login date and time human redable added

--- lib/View/ChatMember.php
+++ lib/View/ChatMember.php
@@ -107,10 +107,13 @@ class View_ChatMember extends \View{
 			$l->current_row_html['chaturl'] = $this->app->url('dashboard',['mode'=>'chatpanel','chatid'=>$l->model->id]);
 
 			if($l->model['chatpanel_last_login_at'] == "0000-00-00 00:00:00" OR $l->model['chatpanel_last_login_at'] == "0000-00-00"){
-				$l->current_row_html['last_login_at'] = " ";
+				$l->current_row_html['last_login_at_wrapper'] = " ";
 			}else{
-				$l->current_row_html['last_login_at'] = "<p> Last Login at ".$this->add('xepan\base\xDate')->diff($this->app->now,$l->model['chatpanel_last_login_at'])."</p>";
+				$l->current_row_html['last_login_at'] = $this->add('xepan\base\xDate')->diff($this->app->now,$l->model['chatpanel_last_login_at']);
 			}
+
+			$l->current_row_html['last_message_created_at'] = $this->add('xepan\base\xDate')->diff($this->app->now,$l->model['last_message_created_at'])."</p>";
+
 		});
 
 		$member_model = $this->add('rakesh\apartment\Model_Member');
@@ -125,6 +128,15 @@ class View_ChatMember extends \View{
 				->setLimit(1);
 			return $q->expr('[0]',[$msg->fieldQuery('description')]);
 		});
+		$member_model->addExpression('last_message_created_at')->set(function($m,$q){
+			$msg = $m->add('rakesh\apartment\Model_MessageSent',['table_alias'=>'lastmsg'])
+				->addCondition([['to_id',$this->app->apartmentmember->id],['to_id',$m->getElement('id')]])
+				->addCondition([['from_id',$this->app->apartmentmember->id],['from_id',$m->getElement('id')]])
+				->setOrder('id','desc')
+				->setLimit(1);
+			return $q->expr('[0]',[$msg->fieldQuery('created_at')]);
+		});
+
 
 		$this->member_lister->setModel($member_model);
 		$this->member_lister->addQuickSearch(['name']);
